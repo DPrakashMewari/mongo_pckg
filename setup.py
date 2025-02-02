ace3a792ee3cc324e74992bc65986f5c252c5661
DPrakashMewari <Prakash.mewari@yahoo.com>
2025-02-02 22:57:11 +0530

    DB code

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
-   
+    long_description = f.read()
 
 __version__ = "0.0.4"
 REPO_NAME = "mongodb_pkg"
-PKG_NAME= "dbauto"
+PKG_NAME = "dbauto"
 AUTHOR_USER_NAME = "prakashM"
 AUTHOR_EMAIL = "prakash.mewari@yahoo.com"
 
@@ -16,15 +15,23 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
+    description="A Python package for connecting with databases.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",  # Correct keyword here
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    )
-
-
+    # packages=find_packages(where="src"),  # Automatically find packages in the src folder
+    install_requires=[  # External dependencies should go here
+        'pymongo',
+        'pymongo[srv]',
+        'dnspython',
+        'pandas',
+        'numpy',
+        'ensure',
+        'pytest',
+    ],
+    python_requires='>=3.6',  # Specify Python version requirements
+)
