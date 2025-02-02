91bdf00f1cfbb3f9cef8ccef9308ac3e85ababa7
DPrakashMewari <Prakash.mewari@yahoo.com>
2025-02-02 23:05:04 +0530

    code

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup, find_packages
-from typing import List
-
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
 
 __version__ = "0.0.4"
 REPO_NAME = "mongodb_pkg"
@@ -16,14 +12,14 @@ setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A Python package for connecting with databases.",
-    long_description=long_description,
+    long_description="Description of the package goes here.",  # Fallback text for testing
     long_description_content_type="text/markdown",  # Correct keyword here
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    # packages=find_packages(where="src"),  # Automatically find packages in the src folder
+    packages=find_packages(where="src"),  # Automatically find packages in the src folder
     install_requires=[  # External dependencies should go here
         'pymongo',
         'pymongo[srv]',
